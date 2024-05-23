8c83a7ca459b2ca93ee8e16f76066edfd2be1035
Hemanth Kodhanda <hemu@outlook.com>
2024-05-23 10:27:03 -0400

    Final

--- cypress/e2e/1_homepage_spec.cy.js
+++ cypress/e2e/1_homepage_spec.cy.js
@@ -1,8 +1,6 @@
 describe('TVOLearn Website Functionality', () => {
 
   const baseUrl = 'https://tvolearn.com';
-  const Grade1PageUrl = '/pages/grade-1';
-  const Grade1MathPageUrl = '/pages/grade-1-mathematics';
 
   before(() => {
     // Set default command timeout
@@ -66,7 +64,7 @@ describe('TVOLearn Website Functionality', () => {
     cy.contains('a', 'Grade 1', { timeout: 10000 }).should('be.visible').click({ multiple: true });
 
     // Check if the page is 404 or opened
-
+    const Grade1PageUrl = '/pages/grade-1';
     cy.url().should('include', Grade1PageUrl).then(url => {
       cy.request({
         url,
@@ -91,7 +89,7 @@ describe('TVOLearn Website Functionality', () => {
     cy.get('.button-math', { timeout: 10000 }).should('be.visible').click({ multiple: true });
 
     // Check if the page is 404 or opened
-
+    const Grade1MathPageUrl = '/pages/grade-1-mathematics';
     cy.url().should('include', Grade1MathPageUrl).then(url => {
       cy.request({
         url,
@@ -110,66 +108,7 @@ describe('TVOLearn Website Functionality', () => {
 
     cy.wait(5000); // Wait for the page to load completely
     // cy.screenshot('after-clicking-math'); // Capture screenshot after clicking mathematics card
-    cy.logActivity('Testing Grade-1 Mathematics page');
   });
-
-  //
-
-
-  const tabs = [
-    { id: '#tab0', title: 'Number' },
-    { id: '#tab1', title: 'Algebra' },
-    { id: '#tab2', title: 'Data' },
-    { id: '#tab3', title: 'Spatial Sense' },
-    { id: '#tab4', title: 'Financial Literacy' }
-  ];
-
-  tabs.forEach((tab) => {
-    it(`Grade-1 Mathematics page: should display the ${tab.title} tab and check its links`, () => {
-      cy.get(`${tab.id} .tabb`).click();
-      cy.get(`${tab.id}`).should('be.visible');
-      cy.get(`${tab.id} .tabla a`).each(($link, index) => {
-        // Click the link, take a screenshot, and navigate back
-        cy.wrap($link).then(($a) => {
-          const href = $a.attr('href');
-          const fullUrl = `${baseUrl}${href}`;
-
-          cy.logActivity(`Visiting: ${fullUrl}`);
-          // cy.logToFile(`Visiting: ${fullUrl}`);
-          //  cy.visit(fullUrl);
-
-          // Make a request to the page URL
-          cy.request({
-            url: fullUrl,
-            failOnStatusCode: false // Prevent Cypress from failing the test on 404
-          }).then((response) => {
-            // Check the status code
-            if (response.status === 404) {
-              cy.logActivity('Error: Page returned a 404 status');
-              //cy.logToFile('Error: Page returned a 404 status');
-              // Optionally, you can fail the test if 404 is found
-              throw new Error(`Page returned a 404 status: ${fullUrl}`);
-            } else {
-              // Visit the page if the status is not 404
-              cy.visit(fullUrl).then(() => {
-                // Capture and log the page title
-                cy.title().then((title) => {
-                  cy.logActivity(`Page Title: ${title}`);
-                  // cy.logToFile('Page Title:', title);
-                  // cy.screenshot(`${tab.title}-link-${index + 1}`);
-                });
-              });
-            }
-          });
-
-          cy.go('back');
-          cy.get(`${tab.id} .tabb`).click();
-        });
-      });
-    });
-  });
-
-
 });
 
 
